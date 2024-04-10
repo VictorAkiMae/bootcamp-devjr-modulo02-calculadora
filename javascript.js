0ca951a46f88db9cb97cb7ae56fff6737ea3e599
VictorAkiMae <maejima508@gmail.com>
2024-04-09 21:52:09 -0300

    closes 4 - Implementar as quatro operações

--- javascript.js
+++ javascript.js
@@ -1,8 +1,27 @@
-function calc(){
+function calc(op){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
 
-    var output = num1 + num2;
+    //var output = num1 + num2;
+
+    //document.getElementById("output").value = output;
+
+    var output = 0;
+
+    switch (op){
+        case '+':
+            output = num1 + num2;
+            break;
+        case '-':
+            output = num1 - num2;
+            break;
+        case '*':
+            output = num1 * num2;
+            break;
+        case '/':
+            output = num1 / num2;
+            break;
+    }
 
     document.getElementById("output").value = output;
 }
